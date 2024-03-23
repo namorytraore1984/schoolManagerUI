ebbc9734e7c8c25ceada8d6c8c72476f63a7988d
Namory Traoré <namorytraore1984@yahoo.fr>
2024-03-23 15:52:47 -0400

    changement des libelés

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    TranslateModule
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
